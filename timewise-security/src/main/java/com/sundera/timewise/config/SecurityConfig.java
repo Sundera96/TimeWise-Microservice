c1505848ef858098a34afbb74ad0581c8fb22b81
Sunderamurthy <sunderamurthy1996@gmail.com>
2024-01-19 22:54:26 -0500

    Added Security using JWT

---
+++ timewise-security/src/main/java/com/sundera/timewise/config/SecurityConfig.java
@@ -0,0 +1,37 @@
+package com.sundera.timewise.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+	
+	@Autowired
+	private JwtAuthenticationFilter jwtFilter;
+	
+	@Autowired
+	private AuthenticationProvider authProvider;
+	
+	@Bean
+	public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+		http.csrf().disable()
+		.authorizeHttpRequests()
+		.requestMatchers("/api/auth/**").permitAll()
+		.anyRequest().authenticated()
+		.and()
+		.sessionManagement()
+		.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+		.and()
+		.authenticationProvider(authProvider)
+		.addFilterBefore(jwtFilter,UsernamePasswordAuthenticationFilter.class);
+		return http.build();
+	}
+}
---
+++ timewise-security/src/main/java/com/sundera/timewise/controller/AuthenticationController.java
@@ -0,0 +1,33 @@
+package com.sundera.timewise.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.sundera.timewise.dto.AuthenticationRequest;
+import com.sundera.timewise.dto.AuthenticationResponse;
+import com.sundera.timewise.dto.RegistrationRequest;
+import com.sundera.timewise.service.AuthenticationService;
+
+@RestController
+@RequestMapping("/api/auth")
+public class AuthenticationController {
+	
+	@Autowired
+	private AuthenticationService authService;
+	
+	@PostMapping("register")
+	public ResponseEntity<AuthenticationResponse> register(@RequestBody RegistrationRequest request){
+		System.out.println("is it reahcing");
+		System.out.println(request.getFirstName());
+		return ResponseEntity.ok(authService.register(request));
+	}
+	
+	@PostMapping("authenticate")
+	public ResponseEntity<AuthenticationResponse> authenticate(@RequestBody AuthenticationRequest request){
+		return ResponseEntity.ok(authService.authenticate(request));
+	}
+}
