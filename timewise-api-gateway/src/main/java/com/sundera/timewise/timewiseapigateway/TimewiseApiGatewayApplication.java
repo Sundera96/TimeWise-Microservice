6f7edd15ca498ba710802c9d9e6173607b8ce3eb
Sunderamurthy <sunderamurthy1996@gmail.com>
2024-01-20 22:08:09 -0500

    Authentication Completed missing- imp validations though

--- timewise-api-gateway/src/main/java/com/sundera/timewise/timewiseapigateway/TimewiseApiGatewayApplication.java
+++ timewise-api-gateway/src/main/java/com/sundera/timewise/timewiseapigateway/TimewiseApiGatewayApplication.java
@@ -3,7 +3,7 @@ package com.sundera.timewise.timewiseapigateway;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "com.sundera.timewise")
 public class TimewiseApiGatewayApplication {
 
 	public static void main(String[] args) {
