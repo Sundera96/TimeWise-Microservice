313b99c48bb9753745bb71fb2b5826b4eb156ad6
Sunderamurthy <sunderamurthy1996@gmail.com>
2024-03-02 21:36:14 -0500

    new service for image storage

---
+++ timewise-mediastorage/timewise-mediastorage/src/main/java/com/sundera/timewisemediastorage/TimewiseMediastorageApplication.java
@@ -0,0 +1,13 @@
+package com.sundera.timewisemediastorage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TimewiseMediastorageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TimewiseMediastorageApplication.class, args);
+	}
+
+}
